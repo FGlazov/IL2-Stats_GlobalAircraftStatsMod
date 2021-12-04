93de629cd9bb25b7720099bc8907b8850413f780
Fedor Glazov <fedorglazov@gmail.com>
2021-12-04 12:16:44 +0100

    Workaround bug where sometimes aircraft was null.

--- src/mod_stats_by_aircraft/background_jobs/streaks_retro_compute.py
+++ src/mod_stats_by_aircraft/background_jobs/streaks_retro_compute.py
@@ -2,6 +2,7 @@ from .background_job import BackgroundJob
 from stats.models import Sortie
 from ..aircraft_mod_models import AircraftBucket
 from ..aircraft_stats_compute import process_streaks_and_best_sorties, get_sortie_type
+from django.db import IntegrityError
 
 
 class StreaksRetroCompute(BackgroundJob):
@@ -13,23 +14,31 @@ class StreaksRetroCompute(BackgroundJob):
 
     def query_find_sorties(self, tour_cutoff):
         return (Sortie.objects.filter(SortieAugmentation_MOD_STATS_BY_AIRCRAFT__computed_max_streaks=False,
-                                      aircraft__cls_base='aircraft', tour__id__gte=tour_cutoff)
+                                      aircraft__cls_base='aircraft', tour__id__gte=tour_cutoff, aircraft__isnull=False)
                 .order_by('-tour__id', 'id'))
 
     def compute_for_sortie(self, sortie):
+        try:
+            buckets = [(AircraftBucket.objects.get_or_create(tour=sortie.tour, aircraft=sortie.aircraft,
+                                                             filter_type='NO_FILTER', player=sortie.player))[0]]
+            filter_type = get_sortie_type(sortie)
+            has_subtype = filter_type != 'NO_FILTER'
+            if has_subtype:
+                buckets.append((AircraftBucket.objects.get_or_create(tour=sortie.tour, aircraft=sortie.aircraft,
+                                                                     filter_type=filter_type, player=sortie.player))[0])
 
-        buckets = [(AircraftBucket.objects.get_or_create(tour=sortie.tour, aircraft=sortie.aircraft,
-                                                         filter_type='NO_FILTER', player=sortie.player))[0]]
-        filter_type = get_sortie_type(sortie)
-        has_subtype = filter_type != 'NO_FILTER'
-        if has_subtype:
-            buckets.append((AircraftBucket.objects.get_or_create(tour=sortie.tour, aircraft=sortie.aircraft,
-                                                                 filter_type=filter_type, player=sortie.player))[0])
-
-        for bucket in buckets:
-            process_streaks_and_best_sorties(bucket, sortie)
-            bucket.update_derived_fields()
-            bucket.save()
+            for bucket in buckets:
+                process_streaks_and_best_sorties(bucket, sortie)
+                bucket.update_derived_fields()
+                bucket.save()
+        # A few sorties pass a null aircraft to a bucket in here.
+        # Not sure why - seems to be an edge case, so we just ignore those sorties.
+        except AircraftBucket.DoesNotExist:
+            sortie.SortieAugmentation_MOD_STATS_BY_AIRCRAFT.computed_max_streaks = True
+            sortie.SortieAugmentation_MOD_STATS_BY_AIRCRAFT.save()
+        except IntegrityError:
+            sortie.SortieAugmentation_MOD_STATS_BY_AIRCRAFT.computed_max_streaks = True
+            sortie.SortieAugmentation_MOD_STATS_BY_AIRCRAFT.save()
 
     def log_update(self, to_compute):
         return '[mod_stats_by_aircraft]: Retroactively computing aircraft streaks. {} sorties left to process.' \
